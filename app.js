ac25efad8219c34a3673ca17d4f5e52fff18edf7
Devesh Shakya <06deveshshakya@gamil.com>
2022-01-28 12:45:22 +0530

    fixed heroku errors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/orders", orderRoute);
 app.use("/checkout", stripeRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client_admin/build"));
+  app.use(express.static(path.join(__dirname, "client_admin", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client_admin", "build", "index.html"));
   });
